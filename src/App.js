4e641c1605160b8ef41cbfb58c34039a7fe98e1c
sohamkamani <sohamkamani@gmail.com>
2016-08-07 22:37:28 +0530

    Added logos

--- src/App.js
+++ src/App.js
@@ -4,12 +4,21 @@ import './App.css'
 import MainChart from './components/MainChart.jsx'
 import DataConfigBar from './components/DataConfigBar.jsx'
 import ChartConfigBar from './components/ChartConfigBar.jsx'
+import githubLogo from './github-logo.svg'
+import ActionHome from 'material-ui/svg-icons/action/home'
+
+const logoStyle = {
+	marginTop: '10px',
+	height: '25px'
+}
 
 class App extends Component {
 	render() {
 		return (
 			<div className="App">
-				<AppBar title="My Data Generator" iconClassNameRight="muidocs-icon-navigation-expand-more"/>
+				<AppBar className="title-bar" title="My Data Generator"
+			iconElementLeft={<a href="http://www.sohamkamani.com/"><ActionHome color="white" style={logoStyle}/></a>}
+			iconElementRight={<a href="https://github.com/sohamkamani/mr-data-generator"><img src={githubLogo} style={logoStyle}/> </a>}/>
 				<div className="content">
 					<div className="primary-container">
 						<ChartConfigBar/>
